49a8b03943ebbbfd4a5173a1951024c98bbbbf6e
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-01-29 16:39:32 -0300

    Tutorial 1

--- answers.sql
+++ answers.sql
@@ -8,3 +8,14 @@ SELECT name, population FROM world
 
 SELECT name, area FROM world
   WHERE area BETWEEN 200000 AND 250000
+
+  -- Tutorial 1
+
+SELECT name FROM world
+  WHERE name LIKE 'Y%'
+
+SELECT name FROM world
+  WHERE name LIKE '%y'
+
+SELECT name FROM world
+  WHERE name LIKE '%x%'

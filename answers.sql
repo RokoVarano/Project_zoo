2409c96db8ae258fa262aa864844ebbf9d66e4bd
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-02-01 13:04:05 -0300

    Tutorial 2 complete

--- answers.sql
+++ answers.sql
@@ -81,6 +81,22 @@ SELECT name FROM world WHERE name LIKE 'United%'
 
 SELECT name, population, area FROM world WHERE area > 3000000 OR population > 250000000
 
-SELECT name, population, area FROM world WHERE (area > 3000000 OR population > 250000000) AND NOT (area > 3000000 AND population > 250000000) 
+SELECT name, population, area FROM world WHERE (area > 3000000 OR population > 250000000) AND NOT (area > 3000000 AND population > 250000000)
 
 SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000, 2) FROM world WHERE continent = 'South America'
+
+SELECT name, ROUND(gdp/population, -3) FROM world WHERE gdp >= 1000000000000 
+
+SELECT name, capital FROM world WHERE LEN(name) = LEN(capital)
+
+SELECT name, capital FROM world WHERE LEFT(name, 1) = LEFT(capital, 1) AND NOT name = capital
+
+SELECT name
+   FROM world
+WHERE name LIKE '%a%'
+AND name LIKE '%e%'
+AND name LIKE '%i%'
+AND name LIKE '%o%'
+AND name LIKE '%u%'
+AND NOT name LIKE '% %'
+

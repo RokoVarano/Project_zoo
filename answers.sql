d336a587508afca108fc332d842108cf8b8536ba
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-02-03 20:00:19 -0300

    Tutorial 9

--- answers.sql
+++ answers.sql
@@ -363,3 +363,49 @@ WHEN dept =3 THEN 'Art'
 ELSE 'None'
 END
 FROM teacher
+
+-- Tutorial 9
+
+SELECT COUNT(*) FROM stops
+
+SELECT id FROM stops WHERE name = 'Craiglockhart'
+
+SELECT id, name FROM stops JOIN route ON stop = id WHERE num = '4' AND company = 'LRT'
+
+SELECT company, num, COUNT(*)
+FROM route WHERE stop=149 OR stop=53
+GROUP BY company, num HAVING COUNT(*) = 2
+
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=53 AND b.stop=149
+
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart'AND stopb.name = 'London Road'
+
+SELECT DISTINCT a.* FROM 
+(SELECT company, num FROM route WHERE stop = 115) AS a
+INNER JOIN
+(SELECT company, num FROM route WHERE stop = 137) AS b
+ON a.num = b.num
+
+SELECT Craig.company, Craig.num FROM
+(SELECT * FROM stops JOIN route ON stop = id WHERE name = 'Craiglockhart') as Craig
+INNER JOIN
+(SELECT * FROM stops JOIN route ON stop = id WHERE name = 'Tollcross') as Toll
+ON Craig.num = Toll.num
+
+SELECT a.name, a.company, a.num FROM 
+(SELECT name, company, num FROM stops JOIN route ON stop = id) as a
+INNER JOIN
+(SELECT num FROM stops JOIN route ON stop = id WHERE name = 'Craiglockhart') as b
+ON a.num = b.num
+WHERE company = 'LRT'
+ORDER BY name
+
+-- missing exercise 10
